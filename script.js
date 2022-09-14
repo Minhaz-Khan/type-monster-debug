0112d403256fade173dd30145d488f9c39e3e3c9
Minhaz-Khan <kminhaz111@gmail.com>
2022-09-14 22:02:38 +0600

    after count value 0 remove the count

--- script.js
+++ script.js
@@ -108,14 +108,13 @@ const start = () => {
   countdownOverlay.style.display = "flex";
 
   const startCountdown = setInterval(() => {
-    countdownOverlay.innerHTML = '<h1>${count}</h1>';
+    countdownOverlay.innerHTML = `<h1>${count}</h1>`;
 
     // finished timer
     if (count == 0) {
       // -------------- START TYPING -----------------
       document.addEventListener("keydown", typeController);
-      countdownOverlay.style.display = "none";
-      debugger;
+      countdownOverlay.style.display = "flex";
       display.classList.remove("inactive");
 
       clearInterval(startCountdown);
